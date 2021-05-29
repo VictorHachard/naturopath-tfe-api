d2ec84d88f83c97467e37b8513e6ffe925cfd7f5
VictorHachard <nicolashachard@gmail.com>
2021-05-29 11:26:16 +0200

    Add resource controller

--- src/main/java/be/heh/app/controller/rest/RestFullController.java
+++ src/main/java/be/heh/app/controller/rest/RestFullController.java
@@ -1,42 +1,32 @@
 package be.heh.app.controller.rest;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.context.support.ServletContextResource;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("api/v1/test/")
+@RequestMapping("api/v1/resource/")
 public class RestFullController {
 
-	/*@GetMapping("/all")
-	public List<String> allAccess() {
-		List<String> l = new ArrayList<>();
-		l.add("Public Content");
-		return l;
-	}
-
-	@GetMapping("/user")
-	@PreAuthorize("hasRole('USER')")
-	public List<String> userAccess() {
-
-		UserSecurity userDetails =
-				(UserSecurity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		List<String> l = new ArrayList<>();
-		l.add("User Content: " + userDetails.getUsername());
-		return l;
-	}
-
-	@GetMapping("/mod")
-	@PreAuthorize("hasRole('OWNER')")
-	public String moderatorAccess() {
-		return "Moderator Board.";
-	}
-
-	@GetMapping("/admin")
-	@PreAuthorize("hasRole('USER')")
-	public String adminAccess() {
-		return "Admin Board.";
-	}*/
+    @ResponseBody
+    @RequestMapping(value = "{url}", method = RequestMethod.GET)
+    public ResponseEntity<FileSystemResource> getImageAsResource(@PathVariable("url") String url) {
+        FileSystemResource res = new FileSystemResource("C:/test/" + url);
+        return new ResponseEntity<FileSystemResource>(res, HttpStatus.CREATED);
+    }
 
 }
