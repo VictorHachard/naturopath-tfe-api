a63e9356b5429af8a6f57f387a3278fc084c61f9
Hachard Victor <31635811+VictorHachard@users.noreply.github.com>
2023-04-08 09:49:37 +0200

    Update for production

--- src/main/java/be/heh/app/Environment.java
+++ src/main/java/be/heh/app/Environment.java
@@ -7,11 +7,11 @@ import java.util.List;
 public class Environment {
 
     //Dev
-    public static final String DATA_FOLDER = "C:/tfe-images/";
-    public static final List<String> ACCESS_CONTROL_ALLOW_ORIGIN_URL = new ArrayList<>(List.of("http://localhost:4200"));
+    //public static final String DATA_FOLDER = "C:/tfe-images/";
+    //public static final List<String> ACCESS_CONTROL_ALLOW_ORIGIN_URL = new ArrayList<>(List.of("http://localhost:4200"));
 
     //Prod
-    //public static final String DATA_FOLDER = "/home/ubuntu/GitHub/tfe-images/";
-    //public static final List<String> ACCESS_CONTROL_ALLOW_ORIGIN_URL = new ArrayList<>(Arrays.asList("http://141.94.251.48", "http://vps.ypc.yt, "http://www.vps.ypc.yt"));
+    public static final String DATA_FOLDER = "/var/www/html/naturopath-data/";
+    public static final List<String> ACCESS_CONTROL_ALLOW_ORIGIN_URL = new ArrayList<>(Arrays.asList("https://naturopath.victorhachard.fr/"));
 
 }
